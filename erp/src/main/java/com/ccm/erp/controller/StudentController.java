bef02c196f81ec08a0bae202bc1077d9acbed18f
surajinlab <suraj.in.lab@gmail.com>
2026-05-27 00:32:34 +0530

    making cross connection between React and Spring Boot

--- erp/src/main/java/com/ccm/erp/controller/StudentController.java
+++ erp/src/main/java/com/ccm/erp/controller/StudentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class StudentController {
 
     @Autowired
