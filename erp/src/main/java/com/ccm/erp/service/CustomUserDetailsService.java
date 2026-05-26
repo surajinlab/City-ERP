dff6d862de8df2dded185d03f92b921b50681c8b
surajinlab <suraj.in.lab@gmail.com>
2026-05-27 00:19:36 +0530

    version 1.0 Adding Security

---
+++ erp/src/main/java/com/ccm/erp/service/CustomUserDetailsService.java
@@ -0,0 +1,62 @@
+package com.ccm.erp.service;
+
+import com.ccm.erp.entity.User;
+import com.ccm.erp.repository.UserRepository;
+
+import org.springframework.security.core.authority.
+        SimpleGrantedAuthority;
+
+import org.springframework.security.core.userdetails.
+        UserDetails;
+
+import org.springframework.security.core.userdetails.
+        UserDetailsService;
+
+import org.springframework.security.core.userdetails.
+        UsernameNotFoundException;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService
+        implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(
+            UserRepository userRepository
+    ) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(
+            String email
+    ) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found"
+                        )
+                );
+
+        return new org.springframework.security.core.userdetails.User(
+
+                user.getEmail(),
+
+                user.getPassword(),
+
+                Collections.singletonList(
+
+                        new SimpleGrantedAuthority(
+                                "ROLE_" + user.getRole()
+                        )
+                )
+        );
+    }
+}
