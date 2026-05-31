d2ab3e991d02c9d640d97953e889f2b036d6447c
surajinlab <suraj.in.lab@gmail.com>
2026-05-31 23:59:08 +0530

    using Data annotation

--- erp/src/main/java/com/ccm/erp/entity/Attendance.java
+++ erp/src/main/java/com/ccm/erp/entity/Attendance.java
@@ -1,8 +1,10 @@
 package com.ccm.erp.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Attendance {
 
     @Id
@@ -15,75 +17,6 @@ public class Attendance {
     private float percentAttendance;
 
     @OneToOne
-    @JoinColumn(name = "rollno", referencedColumnName = "attendanceId")
+    @JoinColumn(name = "rollNo", referencedColumnName = "rollNo")
     private Student student;
-
-    public Long getAttendanceId() {
-        return attendanceId;
-    }
-
-    public void setAttendanceId(Long attendanceId) {
-        this.attendanceId = attendanceId;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public int getTotalClass() {
-        return totalClass;
-    }
-
-    public void setTotalClass(int totalClass) {
-        this.totalClass = totalClass;
-    }
-
-    public int getPresent() {
-        return present;
-    }
-
-    public void setPresent(int present) {
-        this.present = present;
-    }
-
-    public int getAbsent() {
-        return absent;
-    }
-
-    public void setAbsent(int absent) {
-        this.absent = absent;
-    }
-
-    public float getPercentAttendance() {
-        return percentAttendance;
-    }
-
-    public void setPercentAttendance(float percentAttendance) {
-        this.percentAttendance = percentAttendance;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-
-    @Override
-    public String toString() {
-        return "Attendance{" +
-                "attendanceId=" + attendanceId +
-                ", subject='" + subject + '\'' +
-                ", totalClass=" + totalClass +
-                ", present=" + present +
-                ", absent=" + absent +
-                ", percentAttendance=" + percentAttendance +
-                ", student=" + student +
-                '}';
-    }
 }
